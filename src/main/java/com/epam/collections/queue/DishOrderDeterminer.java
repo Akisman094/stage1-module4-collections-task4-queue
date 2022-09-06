ee2446f9d91be350b13de35552bdb729b0ff6476
Akmal Ismoilov <master80098@gmail.com>
2022-09-06 14:50:01 +0500

    Completed Task!

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,31 @@
 package com.epam.collections.queue;
 
+import org.w3c.dom.ranges.Range;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        int i = 0;
+        List<Integer> dishOrder = new ArrayList<>();
+        List<Integer> dishNumbers = new ArrayList<>(IntStream.range(1, numberOfDishes + 1).boxed().collect(Collectors.toList()));
+        while(!dishNumbers.isEmpty())
+        {
+            for(int j = 0; j < dishNumbers.size(); j++)
+            {
+                i++;
+                if(i % everyDishNumberToEat == 0)
+                {
+                    dishOrder.add(dishNumbers.get(j));
+                    dishNumbers.remove(j);
+                    j--;
+                    i = 0;
+                }
+            }
+        }
+        return dishOrder;
     }
 }
